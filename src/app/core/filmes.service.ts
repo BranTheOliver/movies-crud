82bc6f7f7014c1c7740e449edd5479fc62826a10
brantheoliver <bran.oliv22@gmail.com>
2021-03-08 20:19:00 -0300

    Fix: Inserindo melhorias de performance

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ConfigParams } from '../shared/models/config-params';
 import { Filme } from '../shared/models/filme';
+import { ConfigParamsService } from './config-params.service';
 
 const url = 'http://localhost:3000/filmes/';
 
@@ -11,28 +13,16 @@ const url = 'http://localhost:3000/filmes/';
 export class FilmesService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private configService: ConfigParamsService
   ) { }
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
   }
 
-  listar(page: number, limit: number, text: string, gender: string): Observable<Filme[]>{
-    let httpParams = new HttpParams();
-    httpParams = httpParams.set('_page', page.toString());
-    httpParams = httpParams.set('_limit', limit.toString());
-    httpParams = httpParams.set('_sort', 'id');
-    httpParams = httpParams.set('_order', 'desc');
-
-    if (text){
-      httpParams = httpParams.set('q', text);
-    }
-
-    if (gender){
-      httpParams = httpParams.set('genero', gender);
-    }
-
-    return this.http.get<Filme[]>(url, {params: httpParams});
+  listar(config: ConfigParams): Observable<Filme[]>{
+    const configParams = this.configService.configurarParamentros(config);
+    return this.http.get<Filme[]>(url, {params: configParams});
   }
 }
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 import { FilmesService } from 'src/app/core/filmes.service';
+import { ConfigParams } from 'src/app/shared/models/config-params';
 import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
@@ -10,13 +12,16 @@ import { Filme } from 'src/app/shared/models/filme';
 })
 export class ListagemFilmesComponent implements OnInit {
 
-  readonly limit = 8;
-  page = 0;
+  readonly semFoto = 'https://th.bing.com/th/id/R2fa0e781e0ce67556696ff4c6c4b63e5?rik=hBRNVFxNOktXBQ&riu=http%3a%2f%2fwww.casadei.com%2fon%2fdemandware.static%2fSites-casadei-Site%2f-%2fdefault%2fdw4b2b381d%2fimages%2fnoimagezoom.png&ehk=wcv6SXPeSxuaULGIjt2rwDmNSUkUQD3%2fa2ISxDTgOkI%3d&risl=&pid=ImgRaw';
+  
+  config: ConfigParams = {
+    pagina: 0,
+    limite: 8
+  }
   filmes: Filme[] = [];
   filtrosListagem: FormGroup;
   generos: Array<string>;
-  texto: string;
-  genero: string;
+  
 
   constructor(
     private filmesService: FilmesService,
@@ -29,16 +34,18 @@ export class ListagemFilmesComponent implements OnInit {
       genero: ['']
     });
 
-    this.filtrosListagem.get('texto').valueChanges.subscribe(
+    this.filtrosListagem.get('texto').valueChanges
+    .pipe(debounceTime(400))
+    .subscribe(
       (value: string) => {
-        this.texto = value;
+        this.config.pesquisa = value;
         this.resetarConsulta();
       }
     );
 
     this.filtrosListagem.get('genero').valueChanges.subscribe(
       (value: string) => {
-        this.genero = value;
+        this.config.campo = {tipo: 'genero', valor: value};
         this.resetarConsulta();
       }
     );
@@ -62,15 +69,15 @@ export class ListagemFilmesComponent implements OnInit {
   }
 
   private listarFilmes(): void {
-    this.page++;
-    this.filmesService.listar(this.page, this.limit, this.texto, this.genero).subscribe(
+    this.config.pagina++;
+    this.filmesService.listar(this.config).subscribe(
       (filmes: Filme[]) => this.filmes.push(...filmes)
     );
   }
 
   private resetarConsulta(): void{
     this.filmes = [];
-    this.page = 0;
+    this.config.pagina = 0;
     this.listarFilmes();
   }
 
